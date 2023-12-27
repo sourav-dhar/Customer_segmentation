cf90ccc83baa3663853d17dc740e64a0fe3e1876
sourav-dhar <dharsourav03@gmail.com>
2023-12-27 21:11:07 +0530

    created the variables for the training pipelin in the init of constant/training_pipeline which i forgot and updated the config entity with trainingpipelineconfig details

--- RFM_CUST_SEGMENTATION/config/configuration.py
+++ RFM_CUST_SEGMENTATION/config/configuration.py
@@ -7,5 +7,63 @@ from RFM_CUST_SEGMENTATION.constant import *
 from RFM_CUST_SEGMENTATION.utils.utils import read_yaml_file
 
 class Configuration:
+    
     def __init__ (self,
-                  config_file_path:str = CONFIG_FILE_PATH)
+                  config_file_path:str = CONFIG_FILE_PATH,
+                  current_time_stamp:str = CURRENT_TIME_STAMP):
+        try:
+            self.config_info = read_yaml_file(file_path=config_file_path)
+            self.training_pipeline_config = self.get_training_pipeline_config()
+            self.time_stamp = current_time_stamp
+        except Exception as e:
+            CustomException(e,sys)
+        
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
+        try:
+            artifact_dir = self.training_pipeline_config.artifact_dir
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+
+
+
+
+
+def get_training_pipeline_config(self)->TrainingPipelineConfig:
+        try:
+            training_pipeline_config = self.config_info[TRAINING_PIPELINE_CONFIG_KEY]
+
+            artifact_dir = os.path.join(ROOT_DIR,
+                                        training_pipeline_config[TRAINING_PIPLELINE_NAME_KEY],
+                                        training_pipeline_config[TRAINING_PIPELINE_ARTIFACT_DIR_KEY])  
+    
+            training_pipeline_config = TrainingPipelineConfig(artifact_dir=artifact_dir)
+
+            logging.info(f"Training pipeline Config Completed : {training_pipeline_config}")
+
+            return training_pipeline_config
+
+        except Exception as e:
+            raise CustomException(e,sys) from e
+        
--- RFM_CUST_SEGMENTATION/entity/config_entity.py
+++ RFM_CUST_SEGMENTATION/entity/config_entity.py
@@ -1,8 +1,17 @@
 from collections import namedtuple
 
+
+TrainingPipelineConfig = namedtuple("TrainingPipelineConfig",
+                                    [
+                                        "pipeline_name",
+                                        "artifact_dir"
+                                    ])
+
 DataIngestionConfig = namedtuple("DataIngestionConfig",
                                  [
                                     "dataset_download_url",
                                      "raw_data_dir",
                                      "ingested_dir"
                                  ])
+
+
