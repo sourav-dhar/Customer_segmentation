88fc054cfc57dc946aff8d902f7488f88f8655f8
sourav-dhar <dharsourav03@gmail.com>
2023-12-27 20:52:44 +0530

    updated the init of constant file with the details of the config

--- RFM_CUST_SEGMENTATION/config/configuration.py
+++ RFM_CUST_SEGMENTATION/config/configuration.py
@@ -5,3 +5,7 @@ from RFM_CUST_SEGMENTATION.entity.config_entity import *
 from RFM_CUST_SEGMENTATION.entity.artifact_entity import *
 from RFM_CUST_SEGMENTATION.constant import *
 from RFM_CUST_SEGMENTATION.utils.utils import read_yaml_file
+
+class Configuration:
+    def __init__ (self,
+                  config_file_path:str = CONFIG_FILE_PATH)
---
+++ RFM_CUST_SEGMENTATION/constant/__init__.py
@@ -0,0 +1,27 @@
+#to store the log file
+import os
+from datetime import datetime
+
+def get_current_time_stamp():
+    return f"{datetime.now().strftime('%Y-%m-%d %H-%M-%S')}"
+
+CURRENT_TIME_STAMP = get_current_time_stamp()
+ROOT_DIR = os.getcwd()  # to get current working directory
+
+CONFIG_DIR= "config"
+CONFIG_FILE_NAME= "config.yaml"
+SCHEMA_FILE_NAME='schema.yaml'
+
+CONFIG_FILE_PATH= os.path.join(ROOT_DIR, CONFIG_DIR, CONFIG_FILE_NAME)
+SCHEMA_FILE_PATH=os.path.join(ROOT_DIR, CONFIG_DIR, SCHEMA_FILE_NAME)
+
+
+
+#from customer_segment.constant.data_base import *
+from RFM_CUST_SEGMENTATION.constant.training_pipeline import *
+from RFM_CUST_SEGMENTATION.constant.training_pipeline.data_ingestion import *
+#from customer_segment.constant.training_pipeline.data_validation import *
+#from customer_segment.constant.training_pipeline.data_transformation import *
+#from customer_segment.constant.training_pipeline.model_trainer import *
+
+SAVED_MODEL_DIRECTORY='Saved_Model'
