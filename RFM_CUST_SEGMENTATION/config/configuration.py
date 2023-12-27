0ab988b6ceab98cff297bbc7795a9ecdd068cba0
sourav-dhar <dharsourav03@gmail.com>
2023-12-27 18:11:03 +0530

    created the configuration file in the config folder to link all the configs and also created the utils.py and created the helper functions

---
+++ RFM_CUST_SEGMENTATION/config/configuration.py
@@ -0,0 +1,7 @@
+import os, sys
+from RFM_CUST_SEGMENTATION.exception import CustomException
+from RFM_CUST_SEGMENTATION.logger import logging
+from RFM_CUST_SEGMENTATION.entity.config_entity import *
+from RFM_CUST_SEGMENTATION.entity.artifact_entity import *
+from RFM_CUST_SEGMENTATION.constant import *
+from RFM_CUST_SEGMENTATION.utils.utils import read_yaml_file
