a8ead2f472943e459f304f425e3ebaf7135c01ba
sourav-dhar <dharsourav03@gmail.com>
2023-12-28 23:51:46 +0530

    updated the pipeline file for data validation... now we are ready to check the validation pipeline by running the demo.py

--- RFM_CUST_SEGMENTATION/pipeline/pipeline.py
+++ RFM_CUST_SEGMENTATION/pipeline/pipeline.py
@@ -7,8 +7,9 @@ from RFM_CUST_SEGMENTATION.config.configuration import Configuration
 from RFM_CUST_SEGMENTATION.exception import CustomException
 from RFM_CUST_SEGMENTATION.logger import logging
 from RFM_CUST_SEGMENTATION.entity.config_entity import DataIngestionConfig
-from RFM_CUST_SEGMENTATION.entity.artifact_entity import DataIngestionArtifact
+from RFM_CUST_SEGMENTATION.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 from RFM_CUST_SEGMENTATION.components.data_ingestion import DataIngestion
+from RFM_CUST_SEGMENTATION.components.data_validation import DataValidation
 
 class Pipeline:
     def __init__(self, config: Configuration = Configuration()) -> None:
@@ -24,6 +25,15 @@ class Pipeline:
             return data_ingestion.initiate_data_ingestion()
         except Exception as e:
             CustomException(e,sys)
+    
+    #data_validation
+    def start_data_validation(self,data_ingestion_artifact: DataIngestionArtifact ) -> DataValidationArtifact:
+        try:
+            data_validation = DataValidation(data_ingestion_artifact= data_ingestion_artifact,
+                                            data_validation_config= self.config.get_data_validation_config())
+            return data_validation.initiate_data_validation()
+        except Exception as e:
+            raise CustomException(e, sys) from e
             
     #pipeline
     
@@ -32,6 +42,9 @@ class Pipeline:
         try:
             #data ingestion
             data_ingestion_artifact = self.start_data_ingestion()
+            #data_validation
+            data_validation_artifact = self.start_data_validation()
+            
         except Exception as e:
             CustomException(e,sys) 
             
